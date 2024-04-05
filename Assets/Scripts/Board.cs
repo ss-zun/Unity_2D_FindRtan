b53b5c9ec5e567e6fa36a024c06e4fc073dabafb
ss-zun <zxq0158@gmail.com>
2024-04-05 17:41:48 +0900

    카드 배치의 시작 위치 설정

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,9 +8,14 @@ public class Board : MonoBehaviour
 
     void Start()
     {
-        for(int i = 0; i < 16; i++)
+        for (int i = 0; i < 16; i++)
         {
-            Instantiate(card, this.transform);
+            GameObject go = Instantiate(card, this.transform); // 임시 변수
+
+            float x = (i % 4) * 1.4f - 2.1f; // 나머지 * 카드 간격
+            float y = (i / 4) * 1.4f - 3.0f; // 몫 * 카드 간격
+
+            go.transform.position = new Vector2(x, y); // 카드 배치
         }
     }
 
