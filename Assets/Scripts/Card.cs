7d9ffa5e6ea476a19aed723851e5230761ed6212
ss-zun <zxq0158@gmail.com>
2024-04-08 17:56:23 +0900

    카드 뒤집기

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,11 +6,25 @@ public class Card : MonoBehaviour
 {
     int idx = 0;
 
-    public SpriteRenderer front;
+    public GameObject front; // 카드 이미지면
+    public GameObject back;  // 카드 물음표면
 
+    public Animator anim; // Card 프리팹의 애니메이션
+
+    public SpriteRenderer frontImage; // 카드 이미지
+
+    // 카드 랜덤값, 이미지 셋팅
     public void Setting(int number)
     {
-        idx = number;
-        front.sprite = Resources.Load<Sprite>($"rtan{idx}");
+        idx = number; // 카드마다 랜덤값 지정
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}"); // 리소스 이미지 불러오기
+    }
+
+    // 카드가 오픈됐을 때
+    public void OpenCard()
+    {
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
     }
 }
